4747c6a3ea90505ed0e1ee7a6a246e2fe11e3126
anthony <ariberah@soe.uagrm.edu.bo>
2024-11-29 11:50:22 -0400

    ADD: SCRUM-68/SCRUM-105 PROGRAMACION EN LA CAPA DATOS FINALIZADA

---
+++ Capa Datos/DOAProducto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    public class DOAProducto
+    {
+        public static int GuardarProd(EntProducto Prod)
+        {
+            SqlCommand cmd = null;
+            SqlTransaction myTrans = null;
+            ClaseConexion Conexion = new ClaseConexion();
+            SqlConnection cnx = Conexion.conectar();
+            try
+            {
+                cnx.Open();
+                myTrans = cnx.BeginTransaction();
+
+                string sql = "Insert into Producto (Descripcion) values" +
+                    "(@Descripcion);SELECT Scope_Identity();";
+                cmd = new SqlCommand(sql, cnx);
+                cmd.Parameters.AddWithValue("@Descripcion", Prod.Descripcion);
+                cmd.Transaction = myTrans;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                myTrans.Rollback();
+
+            }
+            myTrans.Commit();
+            cnx.Close();
+            return 1;
+        }
+    }
+}
---
+++ CapaEntidad/EntProducto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CapaEntidad
+{
+    public class EntProducto
+    {
+        public int Id_Producto { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
